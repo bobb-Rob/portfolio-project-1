114e31bdd6b8268941c3647c4bef9523f2eb9712
Robertson Akpan <robertsonakpan@gmail.com>
2022-05-10 15:38:50 +0100

    implement the menu close event, style the menu pop up

--- js_files/event.js
+++ js_files/event.js
@@ -1,7 +1,41 @@
-const hamburgerBtn = document.querySelector('.hamburger-btn');
-const navLinksWrapper = document.getElementById('nav-links');
+const executeEvent = () => {
+  function createElement(elem, className) {
+    const closeIcon = document.createElement(elem);
+    closeIcon.classList.add(className);
+    return closeIcon;
+  }
 
-hamburgerBtn.addEventListener('click', () => {
-  navLinksWrapper.style.display = 'flex';
-  navLinksWrapper.lastElementChild.style.display = 'none';
-});
+  const hamburgerBtn = document.querySelector('.hamburger-btn');
+  const navLinksWrapper = document.getElementById('nav-links');
+
+  hamburgerBtn.addEventListener('click', () => {
+    const closeIcon = createElement('img', 'close-icon');
+    const crossWrapper = createElement('li', 'close-icon-wrapper');
+    closeIcon.src = './icons/close-icon.png';
+    closeIcon.alt = 'close icon';
+    crossWrapper.appendChild(closeIcon);
+    navLinksWrapper.appendChild(crossWrapper);
+    navLinksWrapper.style.display = 'flex';
+    navLinksWrapper.lastElementChild.previousElementSibling.style.display =
+      'none';
+
+    closeIcon.addEventListener('click', () => {
+      navLinksWrapper.style.display = 'none';
+      envelopeElem.style.display = 'block';
+      crossWrapper.remove();
+    });
+
+    const navlinks = document.querySelectorAll('li.section-link > a');
+    const navLinksArr = Array.from(navlinks);
+    navLinksArr.forEach((link) => {
+      link.addEventListener('click', () => {
+        navLinksWrapper.style.display = 'none';
+        crossWrapper.remove();
+      });
+    });
+
+    console.log(navLinksArr);
+  });
+};
+
+executeEvent();
