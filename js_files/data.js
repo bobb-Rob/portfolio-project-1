853a91f7af6a6334da8d46b248baaf190d635304
Robertson Akpan <robertsonakpan@gmail.com>
2022-05-12 03:21:20 +0100

    implement mobile and desktop popup

--- js_files/data.js
+++ js_files/data.js
@@ -2,64 +2,64 @@
 
 const projects = [
     {
-        name: 'Profesional Art Printing Data',
+        name: 'Data Dashboard Health Care',
         description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
         featuredImage: "url(./images/project-card-img.svg)",
-        popupImage: "./images/mobilePop-up",
+        popupImage: "./images/mobilePop-up.svg",
         technologies: ['html', 'bootstrap', 'ruby'],
         liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
         sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
-        id: 1
+        id: '1'
+    },
+    {
+        name: 'Website Portfolio',
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
+        featuredImage: "url(./images/project-card-img.svg)",
+        popupImage: "./images/mobilePop-up.svg",
+        technologies: ['html', 'bootstrap', 'ruby'],
+        liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
+        sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
+        id: '2'
     },
     {
         name: 'Profesional Art Printing Data',
         description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
         featuredImage: "url(./images/project-card-img.svg)",
-        popupImage: "./images/mobilePop-up",
+        popupImage: "./images/mobilePop-up.svg",
         technologies: ['html', 'bootstrap', 'ruby'],
         liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
         sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
-        id: 2
+        id: '3'
     },
     {
         name: 'Profesional Art Printing Data',
         description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
         featuredImage: "url(./images/project-card-img.svg)",
-        popupImage: "./images/mobilePop-up",
+        popupImage: "./images/mobilePop-up.svg",
         technologies: ['html', 'bootstrap', 'ruby'],
         liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
         sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
-        id: 3
+        id: '4'
     },
     {
         name: 'Profesional Art Printing Data',
         description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
         featuredImage: "url(./images/project-card-img.svg)",
-        popupImage: "./images/mobilePop-up",
+        popupImage: "./images/mobilePop-up.svg",
         technologies: ['html', 'bootstrap', 'ruby'],
         liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
         sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
-        id: 4
+        id: '5'
     },
     {
         name: 'Profesional Art Printing Data',
         description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
         featuredImage: "url(./images/project-card-img.svg)",
-        popupImage: "./images/mobilePop-up",
+        popupImage: "./images/mobilePop-up.svg",
         technologies: ['html', 'bootstrap', 'ruby'],
         liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
         sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
-        id: 5
-    },
-    {
-        name: 'Profesional Art Printing Data',
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard",
-        featuredImage: "url(./images/project-card-img.svg)",
-        popupImage: "./images/mobilePop-up",
-        technologies: ['html', 'bootstrap', 'ruby'],
-        liveLink: "https://bobb-rob.github.io/portfolio-project-1/",
-        sourceFile: "https://github.com/bobb-rob/portfolio-project-1",
-        id: 6
-    },
+        id: '6'
+    }
     
 ]
--- js_files/event.js
+++ js_files/event.js
@@ -4,26 +4,45 @@ const executeEvent = (() => {
   DOM.hamburgerBtn.addEventListener('click', () => { 
     DOM.createCloseIcon(DOM.navLinksWrapper, 'li');   
     DOM.navLinksWrapper.classList.add('display-f');
-   document.querySelector('.envelope').classList.add('display-n');
-   // Add the click event on the close btn on menu
-    document.querySelector('.close-icon').addEventListener('click', () => {     
-      DOM.navLinksWrapper.classList.remove('display-f');
-      document.querySelector('.envelope').classList.remove('display-n');
+    document.querySelector('.envelope').classList.add('display-n');
+    // Add the click event on the close btn on menu
+    const hamburgerCloseIcon =  document.querySelector('img.close-icon');
+    hamburgerCloseIcon.addEventListener('click', () => {     
+      DOM.navLinksWrapper.classList.toggle('display-f');
+      const mailIcon = document.querySelector('.envelope');
+      mailIcon.classList.toggle('display-n');
       document.querySelector('.close-icon-wrapper').remove();      
     });     
   });
 
    //Attach click events on all the "a" tags both desktop and mobile
-   const navLinksArr = Array.from(document.querySelectorAll('li.insert-event > a'));    
+   const navLinksArr = Array.from(document.querySelectorAll('li.insert-event > a'));
+   console.log(navLinksArr)   
    navLinksArr.forEach((link) => {
-     link.addEventListener('click', function closeMenu() {
-       DOM.navLinksWrapper.classList.remove('display-f'); // Remove display: flex !important class
-       document.querySelector('.envelope').classList.remove('display-n');
-       document.querySelector('.close-icon-wrapper').remove();
+     link.addEventListener('click', () => {
+      if(window.innerWidth < 768){
+        DOM.navLinksWrapper.classList.toggle('display-f'); // Remove display: flex !important class
+      }         
+       console.log('navlink clicked')    
      });
    }); 
    
    
+   const cardBtnArr = Array.from(DOM.projectBtns);   
+   cardBtnArr.forEach((btn) => {     
+     btn.addEventListener('click', (e) => {
+       const project = projects.filter(proj => {        
+         return proj.id === e.target.parentElement.id //Filter the project by id
+        });
+      const i = project[0]; 
+       DOM.workSection.appendChild(DOM.createPopupWindow(i.name,i.technologies, i.popupImage, i.description));
+       
+       DOM.createCloseIcon(document.querySelector('.popup-container'), 'span');
+       document.querySelector('span.close-icon-wrapper').addEventListener('click', () => {
+         document.querySelector('.project-modal').remove();
+       })
+     });
+   });
  
    
 
